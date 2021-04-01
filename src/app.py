884e81049d3b9b252fca3f26ad2c7ff55e72529e
Zachary Khorozian <zkhorozianbc@gmail.com>
2021-04-01 12:50:33 -0400

    fun

---
+++ src/app.py
@@ -0,0 +1,26 @@
+from flask import Flask,request,jsonify
+import hashlib
+
+app = Flask(__name__)
+D = {}
+
+
+@app.route('/messages/<hash_val>')
+def todo(hash_val):
+	if hash_val in D:
+		return jsonify(message=D[hash_val])
+
+	return jsonify(err_msg="Message Not Found"), 404
+    
+@app.route('/messages', methods=['POST'])
+def post_message():
+	message = request.get_json()["message"]
+	hash_val = hashlib.sha256(message).hexdigest()
+
+	D[hash_val] = message
+
+	return jsonify(digest=hash_val)
+	
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
---
+++ src/test.py
@@ -0,0 +1,48 @@
+import requests
+import json
+import hashlib
+import unittest
+
+url = "http://localhost:5000/messages"
+
+def url_for_get(hash_val):
+	return url + "/" + hash_val
+
+def payload_for_post(message):
+	return {"message": message}
+
+
+class TestStringMethods(unittest.TestCase):
+
+	def test_error_response(self):
+		test_message = "unseen"
+		hash_of_test_message = hashlib.sha256(test_message).hexdigest()
+		r1 = requests.get(url_for_get(hash_of_test_message))
+
+		self.assertEqual(r1.status_code,404)
+		self.assertEqual(r1.json()["err_msg"],"Message Not Found")
+
+	def test_post_request(self):
+		test_message = "hello my name is Zachary"
+		hash_of_test_message = hashlib.sha256(test_message).hexdigest()
+		r1 = requests.post(url, json=payload_for_post(test_message))
+
+		self.assertEqual(r1.status_code, 200)
+		self.assertEqual(r1.json()["digest"], hash_of_test_message)
+
+
+	def test_get_request(self):
+		test_message = "hello my name is Zachary"
+		hash_of_test_message = hashlib.sha256(test_message).hexdigest()
+		r1 = requests.get(url_for_get(hash_of_test_message))
+
+
+		self.assertEqual(r1.status_code, 200)
+		self.assertEqual(r1.json()["message"], test_message)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
