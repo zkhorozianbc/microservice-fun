90aa2534bc77c3193ac338bef4db6ac775999e86
Zachary Khorozian <zachary@sundae.com>
2021-08-12 09:15:04 -0400

    comments to func bodies

--- app/app.py
+++ app/app.py
@@ -10,6 +10,14 @@ r = redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"))
 
 @app.route('/messages/<hash_val>')
 def get_message(hash_val):
+	"""
+	Get a previously sent message by its hash value
+
+	Query Params:
+		hash_val: str ()
+
+	"""
+	
 	msg = r.get(hash_val)
 	if not msg:
 		return jsonify(err_msg="Message Not Found"), 404
@@ -22,6 +30,16 @@ def get_message(hash_val):
     
 @app.route('/messages', methods=['POST'])
 def store_message():
+	"""
+	Send message to the message store as json object in request body
+
+	JSON Body:
+		{
+			"message": "This is an example message!"
+		}
+
+	"""
+
 	message = request.get_json()["message"]
 	hash_val = hashlib.sha256(message.encode("utf-8")).hexdigest()
 
